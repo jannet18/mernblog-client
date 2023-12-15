1f40965f4a31835ae615f95be7f97d383b7a9730
jannet18 <janetngei18@gmail.com>
2023-12-15 16:37:22 +0300

    updates

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function login(e) {
+    e.preventDefault();
+    const response = await fetch(`http://localhost:4000/login`, {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.status === 200) {
+      alert("login successful");
+    } else {
+      // alert("login failed");
+      // response.status(400).json("wrong credentials");
+    }
+  }
   return (
-    <form className="login">
+    <form className="login" onSubmit={login}>
       <h1>Login</h1>
-      <input type="text" placeholder="email" />
-      <input type="password" placeholder="password" />
+      <input
+        type="text"
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
       <button>Login</button>
     </form>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,10 +6,13 @@ export const Register = () => {
 
   async function register(e) {
     e.preventDefault();
-    const response = await fetch("http://localhost:4000/register", {
+    const response = await fetch(`http://localhost:4000/register`, {
       method: "POST",
       body: JSON.stringify({ username, password }),
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json, text/plain, */*",
+      },
     });
     // catch errors
     if (response.status === 200) {
@@ -17,17 +20,20 @@ export const Register = () => {
     } else {
       alert("registration failed");
     }
+    // console.log(response);
   }
   return (
     <form action="" className="register" onSubmit={register}>
       <h1>Register</h1>
       <input
+        // autoComplete="current-username"
         type="text"
-        placeholder="email"
+        placeholder="username"
         onChange={(e) => setUsername(e.target.value)}
         value={username}
       />
       <input
+        // autoComplete="current-password"
         type="password"
         placeholder="password"
         onChange={(e) => setPassword(e.target.value)}
